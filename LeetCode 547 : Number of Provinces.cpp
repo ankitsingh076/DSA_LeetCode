43e7c37905ebbe1bed4dc8a6ed506f7832d31292
Ankit Kumar Singh <122385425+ankitsingh076@users.noreply.github.com>
2023-08-21 20:38:22 +0530

    Update LeetCode 547 : Number of Provinces.cpp

--- LeetCode 547 : Number of Provinces.cpp
+++ LeetCode 547 : Number of Provinces.cpp
@@ -1,3 +1,4 @@
+--------------------------------------------------------------------------------------Approach-01 (Using DFS & Creating adjacency list)---------------------------------------------------------------------------------------------------------------------
 class Solution
 {
     public:
@@ -43,4 +44,35 @@ class Solution
         }
         return count;
     }
+};
+
+
+----------------------------------------------------------------------------------Approach-02 (Using DFS & without creating adjacency list)---------------------------------------------------------------------------------------------------------------------
+class Solution
+{
+    public:
+        void dfs(vector<vector < int>> &isConnected, vector<bool> &visited, int u){
+            visited[u]=true;
+
+            for(int v=0;v<isConnected.size();v++){
+                if(!visited[v] && isConnected[u][v]){
+                    dfs(isConnected,visited,v);
+                }
+            }
+        }
+
+        int findCircleNum(vector<vector < int>> &isConnected)
+        {
+            int n = isConnected.size();
+            vector<bool> visited(n, false);
+
+            int count = 0;
+            for(int i=0;i<n;i++){
+                if(!visited[i]){
+                    dfs(isConnected,visited,i);
+                    count++;
+                }
+            }
+            return count;
+        }
 };
