6cd75f16ec50dc0e5c4967082a90a04202921572
Ankit Kumar Singh <122385425+ankitsingh076@users.noreply.github.com>
2023-08-22 19:44:23 +0530

    Update LeetCode 210 : Course Schedule - II.cpp

--- LeetCode 210 : Course Schedule - II.cpp
+++ LeetCode 210 : Course Schedule - II.cpp
@@ -1,3 +1,4 @@
+--------------------------------------------------------------------------------Approach-01 (using BFS)------------------------------------------------------------------------------------------------------------------------
 class Solution
 {
     public:
@@ -53,4 +54,74 @@ class Solution
 
             return {};
         }
+};
+
+
+--------------------------------------------------------------------------------Approach-02 (using DFS)------------------------------------------------------------------------------------------------------------------------
+class Solution
+{
+    public:
+       	// Using DFS
+        bool hasCycle = false;
+
+    void dfs(unordered_map<int, vector < int>> &adj, int u, vector< bool > &visited, vector< bool > &inRecursion, stack< int > &st)
+    {
+        visited[u] = true;
+        inRecursion[u] = true;
+
+        for (int &v: adj[u])
+        {
+            if (!visited[v])
+            {
+                dfs(adj, v, visited, inRecursion, st);
+            }
+            else if (inRecursion[v] == true)
+            {
+                hasCycle = true;
+                return;
+            }
+        }
+        inRecursion[u] = false;
+        st.push(u);
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector < int>> &prerequisites)
+    {
+
+        unordered_map<int, vector < int>> adj;
+
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
+
+        for (int i = 0; i < prerequisites.size(); i++)
+        {
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
+
+            adj[u].push_back(v);
+        }
+
+        stack<int> st;
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (!visited[i])
+            {
+                dfs(adj, i, visited, inRecursion, st);
+            }
+        }
+
+        if (hasCycle)
+        {
+            return {};
+        }
+        vector<int> result;
+        while (!st.empty())
+        {
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        return result;
+    }
 };
